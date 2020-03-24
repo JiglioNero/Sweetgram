1782cbd2e2833c34fc411efe656b9d232f72bdf4
JiglioNero <smouk.chayz@gmail.com>
2020-03-25 00:57:29 +0300

    backup

--- app/src/main/java/com/example/sweetgram/MainActivity.kt
+++ app/src/main/java/com/example/sweetgram/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.sweetgram
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.sweetgram.di.DaggerMainComponent
 import com.example.sweetgram.di.MainComponent
@@ -25,9 +23,6 @@ class MainActivity : AppCompatActivity() {
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
         instance = this
